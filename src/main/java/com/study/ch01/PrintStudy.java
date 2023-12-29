ccdaf2e81f54ed102c752b565fac74d8ecc298e8
kimGuangwoong <155143645+kimGuangwoong@users.noreply.github.com>
2023-12-29 17:28:07 +0900

    Add files via upload

---
+++ src/main/java/com/study/ch01/PrintStudy.java
@@ -0,0 +1,9 @@
+package com.study.ch01;
+
+public class PrintStudy {
+    public static void main(String[] args) {
+        System.out.println("\uAC01");
+        System.out.println(10 + 30);
+        System.out.println("나이: " +(10+20));
+        }
+    }
